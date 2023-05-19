8c80d59c7326814dc2fcd8f6d9480f2e0e3e4c76
Zefinder <jakubiak@insa-toulouse.fr>
2023-05-19 12:21:50 +0200

    Added a few things to manager's doc

--- ppc/src/main/java/ppc/manager/TournamentSolverManager.java
+++ ppc/src/main/java/ppc/manager/TournamentSolverManager.java
@@ -18,11 +18,17 @@ import ppc.event.TournamentSolveEvent;
 import ppc.tournament.output.PdfGenerator;
 import ppc.tournament.solver.LevelThread;
 import ppc.tournament.solver.Solution;
+import ppc.tournament.solver.TournamentSolver;
 
 /**
  * <p>
- * This manager adds level groups to the tournament, launches the solver, and generates the PDF output files.
+ * This manager adds level groups to the tournament, launches the solver, and
+ * generates the PDF output files.
  * </p>
+ * 
+ * @see Manager
+ * @see ppc.annotation.Manager
+ * @see TournamentSolver
  * 
  * @author Sarah Mousset
  *
@@ -31,11 +37,12 @@ import ppc.tournament.solver.Solution;
 public class TournamentSolverManager implements Manager, Listener {
 
 	private static final TournamentSolverManager instance = new TournamentSolverManager();
-	
+
 	private Map<Integer, Map<String, String[][]>> classesByLevel;
 	private LogsManager logs = LogsManager.getInstance();
-	
-	public TournamentSolverManager() {}
+
+	public TournamentSolverManager() {
+	}
 
 	@Override
 	public void initManager() {
@@ -44,14 +51,14 @@ public class TournamentSolverManager implements Manager, Listener {
 
 		this.classesByLevel = new HashMap<>();
 	}
-	
+
 	@EventHandler
 	public void onSolverCalled(TournamentSolveEvent event) {
-		
+
 		int nbClasses = classesByLevel.values().iterator().next().size();
 		List<Thread> threads = new ArrayList<>();
 		List<LevelThread> lvlThreads = new ArrayList<>();
-		
+
 		// so that the levels are in increasing order
 		SortedSet<Integer> keys = new TreeSet<>(classesByLevel.keySet());
 		for (Integer key : keys) {
@@ -59,14 +66,15 @@ public class TournamentSolverManager implements Manager, Listener {
 			int classNb = 0;
 			for (String[][] classes : classesByLevel.get(key).values())
 				lvlClasses[classNb++] = classes;
-			
-			LevelThread lvlThread = new LevelThread(lvlClasses, event.isSoftConstraint(), event.getTimeout(), event.getFirstTable());
+
+			LevelThread lvlThread = new LevelThread(lvlClasses, event.isSoftConstraint(), event.getTimeout(),
+					event.getFirstTable());
 			Thread thread = new Thread(lvlThread);
 			threads.add(thread);
 			lvlThreads.add(lvlThread);
 			thread.start();
 		}
-		
+
 		// waiting for all threads to be done
 		for (Thread thread : threads) {
 			try {
@@ -75,7 +83,7 @@ public class TournamentSolverManager implements Manager, Listener {
 				e.printStackTrace();
 			}
 		}
-		
+
 		// retrieving solutions
 		int lastLevelWithGhost = -1;
 		List<Solution> solutions = new ArrayList<>();
@@ -85,31 +93,31 @@ public class TournamentSolverManager implements Manager, Listener {
 			if (currentSolution.getGhost() != -1)
 				lastLevelWithGhost = lvl;
 		}
-		
+
 		// generating the PDF files
 		String[] classNames = classesByLevel.values().iterator().next().keySet().toArray(new String[0]);
 		PdfGenerator pdfGen = new PdfGenerator(solutions, classNames, nbClasses, lastLevelWithGhost);
 		try {
-			logs.writeInformationMessage("creating pdf ListeMatches... ");
+			logs.writeInformationMessage("Creating pdf ListeMatches... ");
 			pdfGen.createPdfListeMatches();
-			logs.writeInformationMessage("creating pdf ListeClasses... ");
+			logs.writeInformationMessage("Creating pdf ListeClasses... ");
 			pdfGen.createPdfListeClasses();
-			logs.writeInformationMessage("creating pdf ListeNiveaux... ");
+			logs.writeInformationMessage("Creating pdf ListeNiveaux... ");
 			pdfGen.createPdfListeNiveaux();
-			logs.writeInformationMessage("creating pdf FicheProf... ");
+			logs.writeInformationMessage("Creating pdf FicheProf... ");
 			pdfGen.createPdfProfs();
-			logs.writeInformationMessage("creating pdf FichesEleves... ");
+			logs.writeInformationMessage("Creating pdf FichesEleves... ");
 			pdfGen.createPdfEleves();
 		} catch (FileNotFoundException | DocumentException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@EventHandler
 	public void onLevelGroupAdded(TournamentAddLevelGroupEvent event) {
 		this.classesByLevel.put(event.getLevel(), event.getClasses());
 	}
-	
+
 	public static TournamentSolverManager getInstance() {
 		return instance;
 	}
