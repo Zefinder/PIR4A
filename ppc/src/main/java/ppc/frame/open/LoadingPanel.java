2ae746df17d31ec91a074d6eec3a86cfa14694f3
Zefinder <jakubiak@insa-toulouse.fr>
2023-05-24 20:55:55 +0200

    Fixed a little bug with display

--- ppc/src/main/java/ppc/frame/open/LoadingPanel.java
+++ ppc/src/main/java/ppc/frame/open/LoadingPanel.java
@@ -15,7 +15,6 @@ import javax.swing.SwingConstants;
 import ppc.annotation.EventHandler;
 import ppc.event.Listener;
 import ppc.event.SolutionFoundEvent;
-import ppc.manager.EventManager;
 
 public class LoadingPanel extends JPanel implements Listener {
 
@@ -30,15 +29,15 @@ public class LoadingPanel extends JPanel implements Listener {
 	private JButton stopButton;
 
 	public LoadingPanel() {
-		EventManager.getInstance().registerListener(this);
+//		EventManager.getInstance().registerListener(this);
 
 		GridLayout gridLayout = new GridLayout(4, 1);
 		gridLayout.setVgap(10);
 		setLayout(gridLayout);
 		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		
+
 		// TODO: set max panel size
-		
+
 		// TODO: specific level
 		levelLabel = new JLabel("Niveau X");
 		levelLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -61,7 +60,7 @@ public class LoadingPanel extends JPanel implements Listener {
 		});
 		add(stopButton);
 	}
-	
+
 	// TODO: call when final solution found
 	private void searchIsStopped() {
 		// TODO: specific level
@@ -69,27 +68,39 @@ public class LoadingPanel extends JPanel implements Listener {
 		stopButton.setEnabled(false);
 	}
 
-	private void updateProgress(int progressStudents, int progressClasses) {
-		progressBarStudents.setValue(progressStudents);
-		progressBarClasses.setValue(progressClasses);
+	private void updateProgress(float progressStudents, float progressClasses) {
+		progressBarStudents.setString(String.format("%.2f%%", progressStudents));
+		progressBarStudents.setValue((int) progressStudents);
+		progressBarClasses.setString(String.format("%.2f%%", progressClasses));
+		progressBarClasses.setValue((int) progressClasses);
 	}
 
 	@EventHandler
 	public void onSolutionFound(SolutionFoundEvent event) {
-		this.updateProgress(event.getStudentsMet() / event.getMaxStudentsMet() * 100,
-				event.getClassesMet() / event.getMaxClassesMet() * 100);
+		this.updateProgress((float) event.getStudentsMet() / event.getMaxStudentsMet() * 100,
+				(float) event.getClassesMet() / event.getMaxClassesMet() * 100);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		JFrame frame = new JFrame("Loading Panel");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		LoadingPanel loadingPanel = new LoadingPanel();
-		loadingPanel.updateProgress(50, 30);
+		loadingPanel.updateProgress(50f, 30f);
 
 		frame.getContentPane().add(loadingPanel);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+
+		Thread.sleep(2000);
+
+		SolutionFoundEvent event = new SolutionFoundEvent(0, 70, 100, 50, 100);
+		loadingPanel.onSolutionFound(event);
+		
+		Thread.sleep(2000);
+
+		event = new SolutionFoundEvent(0, 100, 100, 86, 100);
+		loadingPanel.onSolutionFound(event);
 	}
 }
